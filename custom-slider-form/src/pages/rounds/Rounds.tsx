077cc18702c9daa29ea9b8e7fb15ef22842e622f
Luciana Diaz Kralj <lucidiaz@itba.edu.ar>
2023-08-01 23:28:00 -0300

    Scroll to top when changing pages

--- custom-slider-form/src/pages/rounds/Rounds.tsx
+++ custom-slider-form/src/pages/rounds/Rounds.tsx
@@ -7,17 +7,23 @@ import '../../App.css'
 
 function Rounds() {
 
-  const { steps, currentStepIdx, step, isFirstStep, isLastStep, next, back } = useMultistepForm([ <RoundOne/>, <RoundTwo/>, <RoundThree/> ])
+  function scrollToTop() {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  };
   
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+};
   return(
     <div>
         <div className='wrapper'>
             <p>{currentStepIdx + 1} / {steps.length}</p>
-            {step}
-            <div className='buttonWrapper mt-3'>
-                { !isFirstStep && <button className='btn btn-primary' onClick={back}>Anterior</button> }
-                { !isLastStep && <button className='btn btn-primary' onClick={next}>Siguiente</button> }
-                { isLastStep && <button className='btn btn-send' onClick={next}>Finalizar</button> }
+            <form onSubmit={handleSubmit}>
+              {step}
+            </form>
+            <div className='button-wrapper mt-3'>
+                { !isFirstStep && <button className='btn btn-primary' onClick={() => {back(); scrollToTop();}}>Anterior</button> }
+                { !isLastStep && <button className='btn btn-primary' onClick={() => {next(); scrollToTop();}}>Siguiente</button> }
+                { isLastStep && <button className='btn btn-send' onClick={() => {next(); scrollToTop();}}>Finalizar</button> }
             </div>
         </div>
     </div>
