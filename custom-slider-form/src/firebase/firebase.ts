6976a73f082af5d33d4e07997a06fa3b1efa5d8e
Luciana Diaz Kralj <lucidiaz@itba.edu.ar>
2023-08-01 23:17:23 -0300

    Config updates

--- custom-slider-form/src/firebase/firebase.ts
+++ custom-slider-form/src/firebase/firebase.ts
@@ -1,36 +1,90 @@
 import { initializeApp } from 'firebase/app';
-import {
-    getAuth,
-    onAuthStateChanged,
-    signOut,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    NextOrObserver,
-    User
-} from 'firebase/auth';
+import { getAuth, onAuthStateChanged, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, NextOrObserver, User, sendPasswordResetEmail } from 'firebase/auth';
+import { getFirestore, query, getDocs, collection, where, doc, setDoc } from "firebase/firestore";
 import { getFirebaseConfig } from './firebaseConfig';
+import { getStorage } from 'firebase/storage';
 
 const app = initializeApp(getFirebaseConfig());
 const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app)
 
-export const signInUser = async (
-    email: string, 
-    password: string
-) => {
-    if (!email && !password) return;
-    return await signInWithEmailAndPassword(auth, email, password)
+export const signInUser = async (email: string, password: string) => {
+    try {
+        return await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+        console.error(err);
+        if (err instanceof Error)
+            alert(err.message);
+        return;
+    }
 }
 
 export const userStateListener = (callback:NextOrObserver<User>) => {
     return onAuthStateChanged(auth, callback)
 }
 
-export const SignOutUser = async () => await signOut(auth);
-
-export const registerUser = async (
-    email: string,
-    password: string
-) => {
-    if (!email && !password) return;
-    return await createUserWithEmailAndPassword(auth, email, password)
+export const signOutUser = async () => {
+    try {
+        await signOut(auth);
+    } catch (err) {
+        console.error(err);
+        if (err instanceof Error)
+            alert(err.message);
+    }
 }
+
+export const registerUser = async (email: string, password: string) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await setDoc(doc(db, "users", user.uid), {
+          uid: user.uid,
+          authProvider: "local",
+          email,
+          progress: 0
+        });
+        return res;
+    } catch (err) {
+        console.error(err);
+        if (err instanceof Error)
+            alert(err.message);
+        return;
+    }
+}
+ 
+const googleProvider = new GoogleAuthProvider();
+export const continueWithGoogle = async () => {
+    try {
+        const res = await signInWithPopup(auth, googleProvider);
+        const user = res.user;
+        const q = query(collection(db, "users"), where("uid", "==", user.uid));
+        const docs = await getDocs(q);
+        if (docs.docs.length === 0) {
+            await setDoc(doc(db, "users", user.uid), {
+                uid: user.uid,
+                name: user.displayName,
+                authProvider: "google",
+                email: user.email,
+                progress: 0
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        if (err instanceof Error)
+            alert(err.message);
+    }
+};
+
+export const sendPasswordReset = async (email: string) => {
+    try {
+        await sendPasswordResetEmail(auth, email);
+        alert("El correo para resetear contraseña ha sido enviado.");
+    } catch (err) {
+        console.error(err);
+        if (err instanceof Error)
+            alert(err.message);
+    }
+};
+
+export { auth, db, storage };
