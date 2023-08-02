4c52b58a5bbb48a7ed0bd88b06ab9890b807b8ef
Luciana Diaz Kralj <lucidiaz@itba.edu.ar>
2023-08-01 23:32:40 -0300

    Updated Rounds

--- custom-slider-form/src/pages/rounds/Rounds.tsx
+++ custom-slider-form/src/pages/rounds/Rounds.tsx
@@ -1,8 +1,13 @@
 
+import { FormEvent, useContext, useEffect, useState } from "react";
+import { doc, updateDoc } from "firebase/firestore";
 import { RoundOne } from '../../pages/rounds/RoundOne';
 import { RoundTwo } from '../../pages/rounds/RoundTwo';
 import { RoundThree } from '../../pages/rounds/RoundThree';
 import { useMultistepForm } from '../../hooks/useMultistepForm';
+import { AuthContext } from "../../context/authContext";
+import { db } from "../../firebase/firebase";
+import { updateForm1, INITIAL_DATA_R1, updateForm2, INITIAL_DATA_R2, updateForm3, INITIAL_DATA_R3, FormDataFields } from "./FormDataFields";
 import '../../App.css'
 
 function Rounds() {
@@ -11,8 +16,54 @@ function Rounds() {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   };
   
+  const { currentUser, userDataRef, setUserDataRef } = useContext(AuthContext);
+  
+  const [data1, setData1] = useState(INITIAL_DATA_R1);
+  const [data2, setData2] = useState(INITIAL_DATA_R2);
+  const [data3, setData3] = useState(INITIAL_DATA_R3);
+  
+  function updateFields1(fields: Partial<FormDataFields>) {
+    setData1(prev => {
+        return { ...prev, ...fields }
+    })
+  }
+
+  function updateFields2(fields: Partial<FormDataFields>) {
+    setData2(prev => {
+        return { ...prev, ...fields }
+    })
+  }
+
+  function updateFields3(fields: Partial<FormDataFields>) {
+    setData3(prev => {
+        return { ...prev, ...fields }
+    })
+  }
+
+  const { steps, currentStepIdx, step, isFirstStep, isLastStep, next, back } = useMultistepForm([ <RoundOne {...data1} updateFields={updateFields1}/>, <RoundTwo {...data2} updateFields={updateFields2}/>, <RoundThree {...data3} updateFields={updateFields3}/> ])
+  
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    try {
+      if (userDataRef != null) {
+        if (currentStepIdx == 0) {
+          updateForm1(userDataRef, data1);
+        } else if (currentStepIdx == 1) {
+          updateForm2(userDataRef, data2);
+        } else if (currentStepIdx == 2) {
+          updateForm3(userDataRef, data3);
+        }
+      } else {
+        alert("Error guardando datos. Intente nuevamente");
+        if (currentUser != null) {
+          setUserDataRef(doc(db, 'users', currentUser.uid));
+        }
+      }
+    } catch (error:any) {
+        console.log('Error en paso '+currentStepIdx, error.message);
+    }
 };
+
   return(
     <div>
         <div className='wrapper'>
